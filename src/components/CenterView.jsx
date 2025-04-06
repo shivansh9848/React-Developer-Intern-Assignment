c6820b59e7defb22bd324d57aa08a5effe0aa9e8
Shivansh9848 <104058319+shivansh9848@users.noreply.github.com>
2025-04-06 20:39:54 +0530

    fix: correct zoom and alignment animation properties in TransformWrapper

--- src/components/CenterView.jsx
+++ src/components/CenterView.jsx
@@ -40,35 +40,36 @@ const CenterView = () => {
 
     return (
         <div ref={containerRef} className="w-full h-full relative">
-          <TransformWrapper
-            initialScale={0.8}
-            minScale={0.5}
-            maxScale={10}
-            initialPositionX={100}
-            initialPositionY={100}
-            minPositionX={-200}
-            maxPositionX={200}
-            minPositionY={-100}
-            maxPositionY={100}
-            wheel={{ disabled: false, step: 0.000001 }}
-            limitToBounds={false}
-            zoomAnimation={{ disabled: false }}
-            alignmentAnimation={{ disabled: true }}
-            zoomAnimationDuration={1000}
-            onPanningStop={({ state }) => updateViewport(state)}
-            onZoomStop={({ state }) => updateViewport(state)}
-          >
-            <Controls />
-      
-            <TransformComponent>
-              <div className="relative bg-gray-200 w-full h-full">
-                <WSIImage imageSrc={image1} detections={detections} />
-              </div>
-            </TransformComponent>
-          </TransformWrapper>
+            <TransformWrapper
+                initialScale={0.8}
+                minScale={0.5}
+                maxScale={10}
+                initialPositionX={100}
+                initialPositionY={100}
+                minPositionX={-200}
+                maxPositionX={200}
+                minPositionY={-100}
+                maxPositionY={100}
+                wheel={{ disabled: false, step: 0.000001 }}
+                limitToBounds={false}
+                zoomAnimation={{ animationTime: 1000 }}
+                alignmentAnimation={{ animaytionTime: 500 }}
+
+                zoomAnimationDuration={1000}
+                onPanningStop={({ state }) => updateViewport(state)}
+                onZoomStop={({ state }) => updateViewport(state)}
+            >
+                <Controls />
+
+                <TransformComponent>
+                    <div className="relative bg-gray-200 w-full h-full">
+                        <WSIImage imageSrc={image1} detections={detections} />
+                    </div>
+                </TransformComponent>
+            </TransformWrapper>
         </div>
-      );
-      
+    );
+
 };
 
 export default CenterView;
